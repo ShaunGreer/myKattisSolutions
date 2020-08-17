3cb6ef955fe103eae78ce4ee7333fcc2abde41e2
Shaun Greer <shaun.greer99@outlook.com>
2020-08-17 20:27:46 +0100

    Java

---
+++ Cooking Water/Main.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        List<List<Integer>> allPossibilities = new ArrayList<>();
+        int biggestDifference = 0;
+        int lowest = 0;
+        int highest = 0;
+        for (int i = 0; i < n; i++) {
+            
+        }
+    }
+}
